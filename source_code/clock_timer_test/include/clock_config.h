f2c596ede006299f9bc22c6baf1227bcd16f71b4
YeetMiche <64234954+YeetMiche@users.noreply.github.com>
2024-06-18 21:40:30 +0200

    added clock_timer_test

---
+++ source_code/clock_timer_test/include/clock_config.h
@@ -0,0 +1,8 @@
+#ifndef CLOCK_CONFIG_H
+#define CLOCK_CONFIG_H
+
+#include "main.h"
+
+void configure_clock();
+
+#endif //CLOCK_CONFIG_H
---
+++ source_code/clock_timer_test/src/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "clock_config.h"
+
+int main(){
+    configure_clock();
+
+
+    return 0;
+}
