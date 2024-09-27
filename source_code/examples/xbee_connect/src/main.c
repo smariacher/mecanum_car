0eeaede468d31ceaa21f26e03dd40bba47681b18
YeetMiche <64234954+YeetMiche@users.noreply.github.com>
2024-09-27 14:01:15 +0200

    changed to Interrupt UART

--- source_code/examples/xbee_connect/src/main.c
+++ source_code/examples/xbee_connect/src/main.c
@@ -1,8 +1,16 @@
 #include "main.h"
 #include "clock_config.h"
+#include "stm32f401xe.h"
 
 #define BAUDRATE 9600
 #define APB_FREQ 16000000
+#define BUFFER_SIZE 128
+
+volatile char rxBuffer[BUFFER_SIZE];
+volatile uint32_t rxIndex = 0;
+
+void USART2_IRQHandler(void);
+void USART2_Init(void);
 
 #define LOG( msg... ) printf( msg );
 
@@ -22,47 +30,75 @@ int _write(int handle, char* data, int size){
     return size;
 }
 
-int main(){
-    // uart2 --> PA2:TX, PA3:RX
-    configure_clock();
-
+void USART2_Init(void){
     // Enable peripheral GPIOA clock
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     // Enable peripheral USART2 clock
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
     // Configure PA2 as USART2_TX using alternate function 7
+    GPIOA->MODER &= ~GPIO_MODER_MODER2_Msk;
     GPIOA->MODER |= GPIO_MODER_MODER2_1;
-    GPIOA->AFR[0] |= 0b0111 << (4*2);
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL2_Msk;
+    GPIOA->AFR[0] |= 7U << GPIO_AFRL_AFSEL2_Pos;
 
     // Configure PA3 as USART2_RX using alternate function 7
+    GPIOA->MODER &= ~GPIO_MODER_MODER3_Msk;
     GPIOA->MODER |= GPIO_MODER_MODER3_1;
-    GPIOA->AFR[0] |= 0b0111 << (4*3);
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL3_Msk;
+    GPIOA->AFR[0] |= 7U << GPIO_AFRL_AFSEL3_Pos;
 
     // Clear CR1 register
     USART2->CR1 = 0;
     // Enable transmitter and receiver
     USART2->CR1 |= (USART_CR1_TE | USART_CR1_RE);
+    
+    // Enable RXNE interrupt
+    USART2->CR1 |= USART_CR1_RXNEIE;
 
     // Set baudrate 
     USART2->BRR = (APB_FREQ / BAUDRATE);
 
     // Enable USART2
     USART2->CR1 |= USART_CR1_UE;
-    int cnt = 0;
 
-    uint8_t rxb = '\0';
+    // Enable USART2 interrupt in NVIC
+    NVIC_SetPriority(USART2_IRQn, 0);
+    NVIC_EnableIRQ(USART2_IRQn);
+}
 
-    for (;;){
-        while (! (USART2->SR & USART_SR_RXNE)) {
-            ;
+void USART2_IRQHandler(void){
+    if (USART2->SR & USART_SR_RXNE){
+        char receivedChar = (char)(USART2->DR & 0xFF);
+
+        if (rxIndex < BUFFER_SIZE - 1){
+            rxBuffer[rxIndex++] = receivedChar;
+
+            // Echo the received character
+            while (!(USART2->SR & USART_SR_TXE));
+            USART2->DR = receivedChar;
+
+            if (receivedChar == '\r'){
+                rxBuffer[rxIndex] = '\0';
+                rxIndex = 0;
+                
+                // Process the received command here
+                LOG("Received: %s\r\n", rxBuffer);
+            }
         }
-        rxb = USART2->DR;
-        
-        LOG("Hello World! %d\n", rxb);
-        delay(100000);
-        cnt++;
+        else {
+            rxIndex = 0;
+        }
     }
+}
 
-    return 0;
+int main(void){
+    // uart2 --> PA2:TX, PA3:RX
+    configure_clock();
+    USART2_Init();
+    LOG("ONLINE\r\n");
+    
+    while (1){
+        // Main program loop
+    }
 }
